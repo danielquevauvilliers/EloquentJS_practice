e45e339a74c8ab1b2c48ecdf012e887866c8b804
Daniel Quevauvilliers <danielquevauvilliers@gmail.com>
2021-09-07 16:47:10 +0400

    add chessboard.js

---
+++ chessboard.js
@@ -0,0 +1,20 @@
+let size = 8;
+let firstRow = "";
+let secondRow = "";
+do {
+    firstRow += " ";
+    secondRow += "#";
+    if (firstRow.length >= size) {
+        break;
+    }
+    firstRow += "#";
+    secondRow += " ";
+} while (firstRow < size);
+
+for (let i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+        console.log(firstRow);
+    } else {
+        console.log(secondRow);
+    }
+}
